2b52d5c261e5998e130f2191ec3ebd31e19b6d0d
to-y <yitong922@gmail.com>
2016-09-22 22:13:12 +0000

    maps changes

--- maps.js
+++ maps.js
@@ -3,6 +3,13 @@
 let arr = ["ground", "control", "to", "major", "tom"];
 
 
+function maps (arr,cb) {
+ return arr.map(cb);
+}
+
+console.log(maps(arr,length));
+
+
 function map (arr, cb) {
   arr.forEach((x) => {
     console.log(cb(x));
